a89e03099b9c31fec87fc05e5d54f1f896a32960
K9-guardian <girkar.km@gmail.com>
2022-09-09 19:02:26 -0700

    Clean up fen parser

--- fen.rkt
+++ fen.rkt
@@ -6,8 +6,8 @@
   (match (string-split fen)
     [(list piece-placement-data active-color castling-availability
            en-passant-target-square halfmove-clock fullmove-number)
-     (list (list 'piece-placement-data (map board-row (string-split piece-placement-data "/")))
-           (list 'active-color (string-ref active-color 0))
+     (list (list 'piece-placement-data (map board-row (reverse (string-split piece-placement-data "/"))))
+           (list 'active-color (hash-ref color->symbol active-color))
            (list 'castling-availability (castling castling-availability))
            (list 'en-passant-target-square (en-passant en-passant-target-square))
            (list 'halfmove-clock (string->number halfmove-clock))
@@ -16,21 +16,42 @@
 (define (castling availability)
   (if (string=? "-" availability)
       '()
-      (let ([castles (string->list availability)])
-        (list (list #\w (map char-downcase
-                             (filter (curryr member '(#\K #\Q)) castles)))
-              (list #\b (filter (curryr member '(#\k #\q)) castles))))))
+      (let*-values ([(castles) (map (curry hash-ref char->piece) (string->list availability))]
+                    [(whites blacks) (partition (λ (piece) (eq? 'white (car piece))) castles)])
+        (map list '(white black) (list (map cadr whites) (map cadr blacks))))))
 
 (define (en-passant target-square)
   (if (string=? "-" target-square)
       '()
-      (let ([coord (string->list target-square)])
-        (list (- (char->integer (car coord)) 97)
-              (- (char->integer (cadr coord)) 49)))))
+      (let ([square (string->list target-square)])
+        (list (hash-ref file->index (car square))
+              (hash-ref rank->index (cadr square))))))
 
 (define (board-row fen-row)
   (append-map (λ (ch)
-                (if (char-numeric? ch)
-                    (make-list (- (char->integer ch) 48) #\_)
-                    (list ch)))
+                (if (hash-has-key? char->number ch)
+                    (make-list (hash-ref char->number ch) 'none)
+                    (list (hash-ref char->piece ch))))
               (string->list fen-row)))
+
+(define color->symbol (hash "w" 'white "b" 'black))
+
+(define char->piece
+  (hasheqv #\P '(white pawn)
+           #\N '(white knight)
+           #\B '(white bishop)
+           #\R '(white rook)
+           #\Q '(white queen)
+           #\K '(white king)
+           #\p '(black pawn)
+           #\n '(black knight)
+           #\b '(black bishop)
+           #\r '(black rook)
+           #\q '(black queen)
+           #\k '(black king)))
+
+(define char->number (hasheqv #\1 1 #\2 2 #\3 3 #\4 4 #\5 5 #\6 6 #\7 7 #\8 8))
+
+(define rank->index (hasheqv #\1 0 #\2 1 #\3 2 #\4 3 #\5 4 #\6 5 #\7 6 #\8 7))
+
+(define file->index (hasheqv #\a 0 #\b 1 #\c 2 #\d 3 #\e 4 #\f 5 #\g 6 #\h 7))
